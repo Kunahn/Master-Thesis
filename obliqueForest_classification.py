aa14c64137a81d0d9684f77393936999a0be5347
Dario <daaeru@gmail.com>
2021-08-08 11:57:17 +0200

    Create obliqueForest_classification.py

---
+++ obliqueForest_classification.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_iris, load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn_oblique_tree.oblique import ObliqueTree
+
+random_state = 2
+
+#see Murthy, et all for details.
+#For oblique with consideration of axis parallel
+#tree = ObliqueTree(splitter="oc1, axis_parallel", number_of_restarts=20, max_perturbations=5, random_state=random_state)
+#
+#For multivariate CART select 'cart' splitter
+#tree = ObliqueTree(splitter="cart", number_of_restarts=20, max_perturbations=5, random_state=random_state)
+
+#consider only oblique splits
+tree = ObliqueTree(splitter="oc1", number_of_restarts=20, max_perturbations=5, random_state=random_state)
+
+X_train, X_test, y_train, y_test = train_test_split(*load_iris(return_X_y=True), test_size=.4, random_state=random_state)
+
+tree.fit(X_train, y_train)
+
+predictions = tree.predict(X_test)
+
+
+print("Iris Accuracy:",accuracy_score(y_test, predictions))
